5a53be59b09e8a1065dd75274a086dd3e2190c6b
SSchnitzler <SSchnitzler@users.noreply.github.com>
2015-12-16 16:55:06 +0100

    Update controller.js
    made sending multiple commands working (quick and semi dirty)

--- controller.js
+++ controller.js
@@ -2,27 +2,30 @@ var fpm = require('./Directives/fpm');
 var main = require('./Directives/default');
 
 module.exports = function(msg, outputChannel) {
-  var workingMsgBody = msg.body.replace('@bmo', '');
-
-  if (workingMsgBody.indexOf("ls") > -1) {
-    main.ls(function(data) {
-      outputChannel.sendOutput(msg.medium, data);
-    });
-  } else if (workingMsgBody.indexOf("fpm status") > -1) {
-    fpm.status(function(data) {
-      outputChannel.sendOutput(msg.medium, data);
-    });
-  } else if (workingMsgBody.indexOf("fpm restart") > -1) {
-    fpm.restart(function(data) {
-      outputChannel.sendOutput(msg.medium, data);
-    });
-  } else if (workingMsgBody.indexOf('help') > -1) {
-    main.help(function(data) {
-      outputChannel.sendOutput(msg.medium, data);
-    });
-  } else {
-    main.default(function(data) {
-      outputChannel.sendOutput(msg.medium, data);
-    });
-  }
+  var workingMsgBody = msg.body.replace('@bmo', '').replace(' ', '');
+  var commands = workingMsgBody.split(";");
+  
+  commands.forEach(function (currentCommand) {
+    if (currentCommand ==="ls") {
+      main.ls(function(data) {
+        outputChannel.sendOutput(msg.medium, data);
+      });
+    } else if (currentCommand === "fpmstatus") {
+      fpm.status(function(data) {
+        outputChannel.sendOutput(msg.medium, data);
+      });
+    } else if (currentCommand === "fpmrestart") {
+      fpm.restart(function(data) {
+        outputChannel.sendOutput(msg.medium, data);
+      });
+    } else if (currentCommand === 'help') {
+      main.help(function(data) {
+        outputChannel.sendOutput(msg.medium, data);
+      });
+    } else {
+      main.default(function(data) {
+        outputChannel.sendOutput(msg.medium, data);
+      });
+    }
+  });
 }
